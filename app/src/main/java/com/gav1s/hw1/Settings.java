e57a63ffa9167e1466e70a47394d4577608a54cd
GaV1S <GaV1S@yandex.ru>
2021-12-06 11:59:11 +0300

    Homework for lesson 7

---
+++ app/src/main/java/com/gav1s/hw1/Settings.java
@@ -0,0 +1,8 @@
+package com.gav1s.hw1;
+
+public class Settings {
+    public static final String SHARED_PREFERENCE_NAME = "ApplicationSettings";
+    public static final String DARK_THEME = "DarkTheme";
+
+    public static boolean isDarkTheme;
+}
